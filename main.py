7fda2f21ed9a3af9675de999e7989f2f28841503
chins-xing <chins-xing@proton.me>
2026-03-06 16:40:38 +0800

    初步完成用户交互（基于数字判断，后期版本将会改为命令形式）

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 #主入口程序
+
 def main_config(): #配置文件读取
     import configparser
     config = configparser.ConfigParser()
     config.read('config.ini',encoding='UTF-8')
 
-    main_controller = config.get('main','windows-config')
-    print(f"获取配置信息:{main_controller}")
+    main_controller = config.get('config_name','config-name')
+    print(f"已读取文件:{main_controller}")
 
 if __name__ == "__main__":
-    main_config()
+    print("[1]读取配置")
+    print("[0]退出程序")
+    while True:
+        user_Input = input(">>>")
+        if user_Input == ("1"):
+            main_config()
+        elif user_Input == ("0"):
+            break
+
 
