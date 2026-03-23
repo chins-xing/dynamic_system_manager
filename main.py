d31b56b3e6daad08f586dbe1c74f4049fba979cd
chins-xing <chins-xing@proton.me>
2026-03-24 05:22:38 +0800

    初步使用cmd模块完善主程序

--- main.py
+++ main.py
@@ -1,52 +1,33 @@
 #主入口程序
+import cmd
+import configparser
+import core.service_controller
+import core.group_policy
+import core.firewall_controller
 
-def main_config(): #配置文件读取
-    import configparser
-    config = configparser.ConfigParser()
-    config.read('config.ini',encoding='UTF-8')
-
-    main_controller = config.get('config_name','config-name')
-    print(f"已读取文件:{main_controller}")
-
-def main_help(): #基础帮助
-    print("-----------------------------------")
-    print("[help]帮助界面")
-    print("[1]读取配置")
-    print("[2]应用配置文件")
-    print("[0]退出程序")
-    print("-----------------------------------")
-
-def main_help_shell(): #help帮助
-    print("-----------------------------------")
-    print("这是帮助界面")
+class shell(cmd.Cmd):
+    prompt = ">>>"
+    intro = "主程序启动"
     
+    def do_config(self,arg):    #配置文件读取
+        config = configparser.ConfigParser()
+        config.read('config.ini',encoding='UTF-8')
+        main_controller = config.get('config_name','config-name')
+        print(f"已读取文件:{main_controller}")    
+
+    def do_SC(self,arg):
+        core.service_controller.test()
+
+    def do_GP(self,arg):
+        core.group_policy.test()
+    
+    def do_FC(self,arg):
+        core.firewall_controller.test()
+
+    def do_help(self,arg):
+        return super().do_help(arg)
+    
+    def do_exit(self,arg):
+        return True
 if __name__ == "__main__": #用户交互
-    while True:
-        main_help()
-        user_Input = input(">>>")
-        if user_Input == ("1"):
-            main_config()
-        elif user_Input == ("help"):
-            main_help_shell()
-        elif user_Input == ("2"):
-            print("-----------------------------------")
-            print("[1]系统服务控制")
-            print("[2]组策略修改")
-            print("[3]防火墙规则管理")
-            print("[0]上一级")
-            print("-----------------------------------")
-            while True:
-                user_Input_2 = input(">>>")
-                if user_Input_2 == ("1"):
-                    import core.service_controller
-                    core.service_controller.test()
-                elif user_Input_2 == ("2"):
-                    import core.group_policy
-                elif user_Input_2 == ("3"):
-                    import core.firewall_controller
-                elif user_Input_2 == ("0"):
-                    break
-        elif user_Input == ("0"):
-            break
-
-
+    shell().cmdloop()
